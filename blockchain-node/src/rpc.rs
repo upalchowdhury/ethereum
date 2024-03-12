ce1e48967368da47ca340353ecafae0cf69907b3
upalchowdhury <upal.chowdhury@outlook.com>
2024-03-12 17:27:15 -0400

    started rpc server

--- blockchain-node/src/rpc.rs
+++ blockchain-node/src/rpc.rs
@@ -1,10 +1,16 @@
 use jsonrpsee::{
-    server::{ServerBuilder, ServerHandle},
-    RpcModule,
+    core::{async_trait, server::rpc_module::Methods, Error},
+    http_server::{HttpServerBuilder, HttpServerHandle},
+    proc_macros::rpc,
 };
 use std::{env, net::SocketAddr, sync::Arc, time::Duration};
 use tokio::{sync::Mutex, task, time};
 
 
 // Start JsonRPC server and register ethereum RPC mehtods to it
-
+// TODO implement database
+pub struct Rpc<DB: Database> {
+    node: Arc<Node<DB>>,
+    handle: Option<HttpServerHandle>,
+    address: SocketAddr,
+}
